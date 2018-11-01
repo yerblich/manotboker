6202de69a0eddbc8b0f1d4e325c86becd96c397c
joseph erblich <yerblich@gmail.com>
2018-11-01 23:22:31 +0200

    removed register

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,17 +4,17 @@
 
 <head>
   <meta charset="utf-8">
-  
+
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>Easy Distribute</title>
-      
- 
- 
+  <title>Manot Boker</title>
+
+
+
   {{-- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> --}}
-  
+
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
@@ -23,11 +23,11 @@
   {{-- <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"> --}}
   <!-- Custom styles for this template-->
  <link href="{{ asset('css/sb-admin.css') }}" rel="stylesheet">
- 
+
       {{-- <script src=//cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.6/highcharts.js charset=utf-8></script>
       <script src=//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js charset=utf-8></script>
        {!! $data['chart']->script() !!} --}}
-      
+
 
 
 </head>

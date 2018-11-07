c194aed7f4f264cecdf838f24717f7362190348f
joseph erblich <yerblich@gmail.com>
2018-11-07 21:37:17 +0200

    added fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,14 @@
     <title>{{ config('APP_NAME', 'ManotBoker') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('storage/js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('storage/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">

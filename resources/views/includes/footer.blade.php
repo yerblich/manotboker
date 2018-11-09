b74228b789387fea54b48580a605dbe2e8db9639
joseph erblich <yerblich@gmail.com>
2018-11-09 13:37:14 +0200

    jquery include

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -33,6 +33,7 @@
       <!-- Bootstrap core JavaScript-->
       <script src="{{ asset('storage/js/app.js') }}"></script>
       <script src="{{ asset('storage/js/sb-admin.js') }}"></script>
+      <script src="{{ asset('storage/js/jquery.min.js') }}"></script>
       <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
       <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>

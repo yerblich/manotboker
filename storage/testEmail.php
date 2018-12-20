b3d22366041461f55a44c042790ad44381777783
joseph erblich <yerblich@gmail.com>
2018-12-20 16:11:34 +0200

    dd

--- storage/testEmail.php
+++ storage/testEmail.php
@@ -1,6 +1,6 @@
 <?php
 
-`cd /home/mog6kcg03uib/manotboker php artisan schedule:run >> /dev/null 2>&1`
+exec('cd /home/mog6kcg03uib/manotboker php artisan schedule:run >> /dev/null 2>&1');
 //ss
 
  ?>

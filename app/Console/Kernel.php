11f235e6f82aa9f6443840c5c4da48edc7008b8b
joseph erblich <yerblich@gmail.com>
2018-12-20 15:28:11 +0200

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+ use Illuminated\Console\WithoutOverlapping;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,7 +26,15 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('command:testEmail')
-                 ->everyMinute();
+        ->appendOutputTo(storage_path() . '/queue.log')
+                 ->everyMinute()->withoutOverlapping();
+
+
+            //      $schedule->command('queue:work --daemon --once  ')
+            //
+            // ->name('some_name')
+            // ->withoutOverlapping()->everyMinute();
+
     }
 
     /**
